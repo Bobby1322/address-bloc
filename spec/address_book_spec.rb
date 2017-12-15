cb2127904d1fa1d3e205167cfbc8dc5d985f5954
Bob-e <bob.e.mercer@gmail.com>
2017-12-15 10:49:45 -0500

    Assignment 4: Completed

--- spec/address_book_spec.rb
+++ spec/address_book_spec.rb
@@ -36,4 +36,21 @@ RSpec.describe AddressBook do
             expect(new_entry.email).to eq('augusta.king@lovelace.com')
         end
     end
+
+    describe "#remove_entry" do
+        it "removes an entry from the address book" do
+            book = AddressBook.new
+            book.add_entry('Flex Armstrong', '010.012.1816', 'Flex.Armstrong@MS.com')
+            book.add_entry('Ada Lovelace', '010.012.1815', 'augusta.king@lovelace.com')
+            
+            expect(book.entries.size).to eq 2
+            name = "Ada Lovelace"
+            phone_number = "010.012.1815"
+            email = "augusta.king@lovelace.com"
+            book.remove_entry(name, phone_number, email)
+            expect(book.entries.size).to eq 1
+            expect(book.entries.first.name).to eq("Flex Armstrong")
+        end 
+    end
+
 end
